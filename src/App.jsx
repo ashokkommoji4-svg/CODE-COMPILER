6b9f93689f5b18d4248fd704be05d23874ead05c
ashokkommoji4-svg <ashokkommoji4@gmail.com>
2026-04-21 15:43:30 +0530

    Connected frontend to backend

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
 } from 'lucide-react';
 import TerminalComponent from './components/Terminal';
 
-const WS_BASE_URL = 'ws://127.0.0.1:8000/ws/compiler/execute/';
+const WS_BASE_URL = import.meta.env.VITE_API_URL.replace('http', 'ws') + '/ws/compiler/execute/';
 
 const LANGUAGE_CONFIGS = {
   python: {
